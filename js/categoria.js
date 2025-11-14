2cac6341b60853916473e8edbe7a14032e1a8b16
Mickaelzs <mickaelcosta431@gmail.com>
2025-11-14 00:19:00 -0300

    update images

--- js/categoria.js
+++ js/categoria.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       nome: "Tintas para Paredes",
       produtos: [
-        { nome: "Suvinil Toque Fosco Completo", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/303/image/display/34/1.webp" },
-        { nome: "Suvinil Toque Fosco", preco: 144.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/305/image/display/34/1.webp" },
-        { nome: "Suvinil Rende & Cobre Muito", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/78/image/display/34/1.webp" },
-        { nome: "Suvinil Toque Seda", preco: 142.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/311/image/display/34/1.webp" },
+        { nome: "Suvinil Toque Fosco Completo", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/303/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T02%3A29%3A15Z&se=2025-11-15T02%3A34%3A15Z&sr=b&sp=r&sig=iYeUvnavGnyDlBwQI78cY7TcgrGoxaCkP7%2FUndlv%2FgA%3D" },
+        { nome: "Suvinil Toque Fosco", preco: 144.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/305/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T01%3A30%3A20Z&se=2025-11-15T01%3A35%3A20Z&sr=b&sp=r&sig=fkSgZ9uryntMFpqr91QjCO1hnOqIKLbMQ15eTHsYZ%2Fg%3D" },
+        { nome: "Suvinil Rende & Cobre Muito", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/78/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T00%3A44%3A57Z&se=2025-11-15T00%3A49%3A57Z&sr=b&sp=r&sig=7rxpK6BH6dG8h30EUhwv41AYdv88QjZtPK%2Bw%2Fg4Bbqo%3D"},
+        { nome: "Suvinil Toque Seda", preco: 142.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/311/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T00%3A56%3A41Z&se=2025-11-15T01%3A01%3A41Z&sr=b&sp=r&sig=Xg%2FaRuWuJqK3%2Fn2SQ2lD%2Bpyyt6r%2B8hXP9A8Bns7QYYg%3D"},
 
         { nome: "Cimento Queimado", preco: 129.90, imagem: "https://iraja.com.br/products/cimento-queimado/25KG.png" },
         { nome: "Esmalte Base Água", preco: 169.90, imagem: "https://iraja.com.br/products/esmalte-base-agua/3,6L.png" },
@@ -22,8 +22,8 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       nome: "Tintas para Madeiras",
       produtos: [
-        { nome: "Esmalte Suvinil Cor & Proteção Acetinado Base Solvente", preco: 199.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/40/image/display/34/1.webp" },
-        { nome: "Esmalte Suvinil Cor & Proteção Brilhante Base Água", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/37/image/display/34/1.webp" },
+        { nome: "Esmalte Suvinil Cor & Proteção Acetinado Base Solvente", preco: 199.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/40/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T01%3A56%3A25Z&se=2025-11-15T02%3A01%3A25Z&sr=b&sp=r&sig=Z3kqGd9AY4yf5tY22%2B%2BsqlLS5%2FKMbTcDgumKQ%2FXhySI%3D" },
+        { nome: "Esmalte Suvinil Cor & Proteção Brilhante Base Água", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/37/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T01%3A46%3A25Z&se=2025-11-15T01%3A51%3A25Z&sr=b&sp=r&sig=yZ98lRy53HcZa160A%2FsnXHS8rgeql9cFUjL2CoXPhUI%3D" },
         { nome: "Epóxi Base Água", preco: 149.90, imagem: "https://sherwin.com.br/wp-content/uploads/sites/11/2022/06/06-SW-Epoxi-Base-Agua-ECO-36L.png" },
         { nome: "Complementos para Madeira Fundo Branco Fosco", preco: 199.90, imagem: "https://sherwin.com.br/wp-content/uploads/sites/11/2022/06/complementos-para-madeira-fundo.png" },
         { nome: "Sparlack Cetol Stain Beleza Natural", preco: 139.90, imagem: "https://mkpcoral.vtexassets.com/arquivos/ids/586928-1600-auto" },
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
       produtos: [
         { nome: "Coralit Secagem Rápida Acetinado Branco", preco: 139.90, imagem: "https://mkpcoral.vtexassets.com/arquivos/ids/451782/Coral-Coralit-Secagem-Rapida-Galao.png" },
         { nome: "Coralit Total Acetinado Branco", preco: 149.90, imagem: "https://mkpcoral.vtexassets.com/arquivos/ids/621737/Coral-Coralit-Total-Galao.png.png" },
-        { nome: "Esmalte Suvinil Cor & Proteção Grafite Fosco", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/42/image/display/34/1.webp" },
+        { nome: "Esmalte Suvinil Cor & Proteção Grafite Fosco", preco: 139.90, imagem: "https://digitalprdaddc4dsa.blob.core.windows.net/digital-products-img/products/42/image/display/34/1.webp?sv=2025-11-05&spr=https&st=2025-11-14T01%3A59%3A26Z&se=2025-11-15T02%3A04%3A26Z&sr=b&sp=r&sig=jMqqMvN%2FqsJ3LrBPCdrlCwxV2P1F0X6%2BWHK7txXMol4%3D" },
         { nome: "Super Galvite", preco: 149.90, imagem: "https://sherwin.com.br/wp-content/uploads/sites/11/2022/06/super-galvite.png" },
         { nome: "Esmalte Sintético Tradicional Fosco", preco: 139.90, imagem: "https://sherwin.com.br/wp-content/uploads/sites/11/2022/06/esmalte-sintetico-tradicional.png" }
       ]
